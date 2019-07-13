22f8db8b07b50a542e9f44ba075f6089d9753343
Ville Pekkala <ville.pekkala@hotmail.com>
2019-07-13 11:17:03 +0300

    Harjoitus 1.3 suoritettu

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -12,21 +12,18 @@ const Header = (props) => {
     )
 }
 
-const Content = () => {
-    const part1 = "Fundamentals of React";    
-    const part2 = "Using props to pass data";    
-    const part3 = "State of a component";    
+const Part = (props) => {
+    return (
+        <p>{props.part} {props.exercises}</p>
+    )
+}
+
+const Content = (props) => {    
     return (
         <div>
-            <p>
-                {part1} {exercises}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
+            <Part part={props.part1.name} exercises={props.part1.exercises} />
+            <Part part={props.part2.name} exercises={props.part2.exercises} />
+            <Part part={props.part3.name} exercises={props.part3.exercises} />
         </div>
     )
 }
@@ -40,10 +37,23 @@ const Total = () => {
 }
 
 const App = () => {
+    const course = "Half Stack application development";
+    const part1 = {
+        name: "Fundamentals of React",
+        exercises: 10
+    }
+    const part2 = {
+        name: "Using props to pass data",
+        exercises: 7
+    }
+    const part3 = {
+        name: "State of a component",
+        exercises: 14
+    }
     return (
         <div>
             <Header course={course}/>
-            <Content />
+            <Content part1={part1} part2={part2} part3={part3} />
             <Total />
         </div>
     )
