382a2e7c5ba7b848ac1a42fbe8890e40bda03f65
Ville Pekkala <ville.pekkala@hotmail.com>
2019-07-23 20:45:23 +0300

    Osa 1 suoritettu

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    {index:0,text:'If it hurts, do it more often', votes:0},
+    {index:1,text:'Adding manpower to a late software project makes it later!', votes:0},
+    {index:2,text:'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes:0},
+    {index:3,text:'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', votes:0},
+    {index:4,text:'Premature optimization is the root of all evil.', votes:0},
+    {index:5,text:'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', votes:0}
 ]
 
 const Button = (props) => (
@@ -48,14 +48,37 @@ const Statistics = (props) => {
     )
 }
 
-const Anecdote = () => {
-    let rand = Math.random();
-    rand = Math.floor(rand * 10)  % anecdotes.length;
+const Anecdote = (props) => {    
     return (
-        <div>{anecdotes[rand]}</div>
+      <div>
+        <div>{props.anecdote.votes.toString()}</div>
+        <div>{props.anecdote.text.toString()}</div>
+      </div>
     )
+}
 
+const MostVotedAnectode = (props) => {
+  if (props.anecdote === undefined) return (<div>No voted anectodes yet</div>)
+  else return (<div>    
+    <div>{props.anecdote.text.toString()}</div>
+    <div>has {props.anecdote.votes.toString()} votes</div>
+  </div>)
+}
 
+function getAnecdote() {
+  let rand = Math.random();
+  rand = Math.floor(rand * 10)  % anecdotes.length;
+  return anecdotes[rand];
+}
+
+function setAnecdote(currentAnecdote, anecdoteSetter) {
+  let tempAnecdote;
+  do {
+    tempAnecdote = getAnecdote();
+    console.log(tempAnecdote)
+  } while (tempAnecdote.text === currentAnecdote.text);
+  
+  return () => {anecdoteSetter(tempAnecdote);}
 }
 
 const App = () => {
@@ -63,8 +86,20 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [anecdote, anecdoteSetter] = useState(anecdotes[0]);
+  const [mostVotedAnect, setMostVotedAnect] = useState(undefined);
     
-  const all = good + neutral + bad;  
+  const all = good + neutral + bad;
+
+  const addVote = () => {
+    anecdotes[anecdote.index].votes += 1;
+    anecdoteSetter({...anecdotes[anecdote.index]});
+    let mostVotes = anecdotes[0];
+    anecdotes.forEach(element => {
+      if (element.votes > mostVotes.votes) mostVotes = element;
+    });
+    setMostVotedAnect(mostVotes);
+  }
 
   return (
     <div>
@@ -75,9 +110,13 @@ const App = () => {
         <Button text="Bad" click={() => setBad(bad + 1)} />
       </div>
       <h2>Statistics</h2>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>      
       <h2>Anecdote</h2>
-      <Anecdote />
+      <Button text="Next anecdote" click={setAnecdote(anecdote, anecdoteSetter)} />
+      <Button text="Vote" click={addVote} />
+      <Anecdote anecdote={anecdote}/>
+      <h2>Anecdote with most votes</h2>
+      <MostVotedAnectode anecdote={mostVotedAnect}/>      
     </div>
   )
 }
